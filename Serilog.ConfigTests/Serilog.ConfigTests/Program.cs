ecc00e2de4edacfc654167923a7aba158bdd510e
Scopla <christoph.schnoetzlinger@gmx.at>
2024-10-05 10:11:58 +0200

    init

---
+++ Serilog.ConfigTests/Serilog.ConfigTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+Log.Information("Hello, world!");
+
+Log.CloseAndFlush();
